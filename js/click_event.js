50c54e6fe7b9fe4d30ad958610d5369ed1e5d737
unknown <13inu.o0@gmail.com>
2025-08-18 17:37:56 +0900

    비주얼 오더버튼 폰트 수정

--- js/click_event.js
+++ js/click_event.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+    // 상단메뉴 호버_서브메뉴
   const menus = this.document.querySelector(".menubar");
   const subMenu = this.document.querySelectorAll(".submenu");
   const hBg = this.document.querySelector(".header-bg");
@@ -15,4 +16,21 @@ window.addEventListener("load", function () {
     })
     hBg.classList.remove("open")
   })
+
+//   스와이퍼 (광고배너)
+const visualSwiper = new Swiper(".banner-swiper", {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
