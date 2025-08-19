b8cfc8e44d29d46ead586492aa1356c02b8feb81
unknown <13inu.o0@gmail.com>
2025-08-19 15:24:21 +0900

    호버아이콘 추가

--- js/click_event.js
+++ js/click_event.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    // 상단메뉴 호버_서브메뉴
+  // 상단메뉴 호버_서브메뉴
   const menus = this.document.querySelector(".menubar");
   const subMenu = this.document.querySelectorAll(".submenu");
   const hBg = this.document.querySelector(".header-bg");
@@ -10,15 +10,15 @@ window.addEventListener("load", function () {
     });
     hBg.classList.add("open");
   });
-  menus.addEventListener("mouseleave", function(){
-    subMenu.forEach(function (sub){
-        sub.classList.remove("active")
-    })
-    hBg.classList.remove("open")
-  })
+  menus.addEventListener("mouseleave", function () {
+    subMenu.forEach(function (sub) {
+      sub.classList.remove("active");
+    });
+    hBg.classList.remove("open");
+  });
 
-//   스와이퍼 (광고배너)
-const visualSwiper = new Swiper(".banner-swiper", {
+  //   스와이퍼 (광고배너)
+  const visualSwiper = new Swiper(".banner-swiper", {
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
@@ -34,7 +34,7 @@ const visualSwiper = new Swiper(".banner-swiper", {
     },
   });
 
-    // 샌드위치 메뉴 스와이퍼
+  // 샌드위치 메뉴 스와이퍼
   const itemSwiper = new Swiper(".food-menu", {
     navigation: {
       nextEl: ".swiper-button-next",
@@ -45,14 +45,25 @@ const visualSwiper = new Swiper(".banner-swiper", {
     spaceBetween: 30,
     breakpoints: {
       590: {
-        slidesPerView: 2.5,
+        slidesPerView: 2,
       },
       768: {
-        slidesPerView: 3.5,
+        slidesPerView: 3,
       },
       1200: {
         slidesPerView: 4,
       },
     },
   });
+
+  // 호버 줌아이콘 실행
+  const sandwich = this.document.querySelectorAll(".swiper a");
+  const zoomIcon = this.document.querySelector(".zoom-icon");
+  sandwich.addEventListener("mouseenter", function () {
+    sandwich.forEach(function (sand) {
+      sand.classList.add("hover");
+    });
+    zoo
+  });
+  zoomIcon;
 });
